cdc8d808f8be1d4151125721a5442e5f69514e9b
Santiago Sommer <santisommer52@gmail.com>
2022-07-24 11:59:06 -0300

    fab

--- h/Fabrica.h
+++ h/Fabrica.h
@@ -1,9 +1,14 @@
 #pragma once
 
+class IReloj;
+class IUsuarios;
+class IReservas;
+class IHostales;
+
 class Fabrica{
     public:
         virtual IReloj getIReloj() = 0;
         virtual IUsuarios getIUsuarios() = 0;
-        virtual IReloj getIReservas() = 0;
+        virtual IReservas getIReservas() = 0;
         virtual IHostales getIHostales() = 0;
 }
